ba32a5d7391a9958351362f01cb6057c6c85adc0
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-14 21:57:38 +0600

    add script.js file

---
+++ script.js
@@ -0,0 +1,3 @@
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
